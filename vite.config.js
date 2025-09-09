28bd8faee0bdfbcc914697c3673e50afb93eca3b
Daniel  Hatton <spanno11@live.com>
2025-09-09 21:30:03 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true
+  root: '.', // Root directory
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-}) 
+      '@': '.'
+    }
+  }
+})
