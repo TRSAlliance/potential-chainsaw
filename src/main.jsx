c69eefa43a4e4542d0c84fc9af894145a8e92aa1
Daniel  Hatton <spanno11@live.com>
2025-10-30 02:27:11 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Landing from "./pages/Landing.jsx";
-import AppRoute from "./pages/AppRoute.jsx";
-
-import "./index.css";
+import Landing from "./pages/Landing";
+import Intro from "./pages/Intro";
+import AppDashboard from "./pages/AppDashboard";
+import LearningProgressHub from "./LearningProgressHub";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/app" element={<AppRoute />} />
+      <Route path="/intro" element={<Intro />} />
+      <Route path="/app" element={<AppDashboard />} />
+      <Route path="/learning" element={<LearningProgressHub userProgress={{}} />} />
     </Routes>
   </BrowserRouter>
 );
